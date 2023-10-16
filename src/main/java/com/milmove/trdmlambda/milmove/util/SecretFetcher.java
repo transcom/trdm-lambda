f5db0ebd9d3dfa88805762357f0461cac6eb315a
cameroncaci <cameron.jewell@caci.com>
2023-10-16 13:24:32 -0400

    Improved log handling within secret fetcher

--- src/main/java/com/milmove/trdmlambda/milmove/util/SecretFetcher.java
+++ src/main/java/com/milmove/trdmlambda/milmove/util/SecretFetcher.java
@@ -1,7 +1,9 @@
 package com.milmove.trdmlambda.milmove.util;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import ch.qos.logback.classic.Logger;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.ssm.SsmClient;
@@ -10,28 +12,38 @@ import software.amazon.awssdk.services.ssm.model.GetParameterResponse;
 
 @Component
 public class SecretFetcher {
+    private Logger logger = (Logger) LoggerFactory.getLogger(SecretFetcher.class);
     private static final String ENVIRONMENT_CONTEXT = "/trdm-lambda/";
     private static final String AWS_REGION = "us-gov-west-1";
     private final SsmClient ssmClient;
 
     public SecretFetcher() {
-        Region region = Region.of(AWS_REGION);
-
-        this.ssmClient = SsmClient.builder()
-                                  .region(region)
-                                  .credentialsProvider(DefaultCredentialsProvider.create())
-                                  .build();
+        try {
+            Region region = Region.of(AWS_REGION);
+    
+            this.ssmClient = SsmClient.builder()
+                                      .region(region)
+                                      .credentialsProvider(DefaultCredentialsProvider.create())
+                                      .build();
+        } catch (Exception e) {
+            logger.error("Error initializing ssmClient: " + e.getMessage());
+            throw new RuntimeException("Failed to initialize ssmClient", e);
+        }
     }
 
     public String getSecret(String secretName) {
-        String fullSecretName = ENVIRONMENT_CONTEXT + secretName;
-
-        GetParameterRequest request = GetParameterRequest.builder()
-                                                         .name(fullSecretName)
-                                                         .withDecryption(true)
-                                                         .build();
-
-        GetParameterResponse response = ssmClient.getParameter(request);
-        return response.parameter().value();
+        try {
+            String fullSecretName = ENVIRONMENT_CONTEXT + secretName;
+            GetParameterRequest request = GetParameterRequest.builder()
+                                                             .name(fullSecretName)
+                                                             .withDecryption(true)
+                                                             .build();
+            GetParameterResponse response = ssmClient.getParameter(request);
+            return response.parameter().value();
+        } catch (Exception e) {
+            logger.error("Error when fetching secret: " + e.getMessage());
+            throw new RuntimeException("Failed to retrieve secret", e);
+        }
     }
+    
 }
